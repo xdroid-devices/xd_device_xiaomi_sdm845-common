722417f5219d07cfc282345a8851975c134e1df9
Kyuofox <88403766@qq.com>
2022-05-08 00:01:13 +0800

    sdm845-common: Merge configs to power-libperfmgr HAL
    Signed-off-by: Kyuofox <88403766@qq.com>

--- sdm845.mk
+++ sdm845.mk
@@ -304,7 +304,7 @@ PRODUCT_PACKAGES += \
     android.hardware.power-service.xiaomi-libperfmgr
 
 PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/power/configs/powerhint.json:$(TARGET_COPY_OUT_VENDOR)/etc/powerhint.json
+    $(LOCAL_PATH)/power-libperfmgr/configs/powerhint.json:$(TARGET_COPY_OUT_VENDOR)/etc/powerhint.json
 
 # Protobuf
 PRODUCT_PACKAGES += \
