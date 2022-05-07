f1a7f0c66632bb2a4b0738cc3a8ac6203459eb80
Kyuofox <88403766@qq.com>
2022-05-07 23:20:27 +0800

    sdm845-common: bring WfdCommon and IFAA back in tree
    Looks like the platform-bootclasspath in non-root soong namespace
    issue has been fixed.
    
    Revert "sdm845-common: move IFAA to another repo"
    
    This reverts commit 29157abbbc7bf941140bac72a7d914cec85fb859.
    
    Revert "sdm845-common: build WfdCommon from another repo"
    
    This reverts commit 202310cd56cc350b3cede9d901bddce750e883a3.
    
    Signed-off-by: Kyuofox <88403766@qq.com>

--- sdm845.mk
+++ sdm845.mk
@@ -219,7 +219,11 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/permissions/privapp-permissions-hotword.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/permissions/privapp-permissions-hotword.xml
 
 # IFAA manager
-include vendor/xiaomi/sdm845-common-extra/ifaa.mk
+PRODUCT_PACKAGES += \
+    org.ifaa.android.manager
+
+PRODUCT_BOOT_JARS += \
+    org.ifaa.android.manager
 
 # Input
 PRODUCT_COPY_FILES += \
@@ -421,4 +425,5 @@ PRODUCT_PACKAGES += \
     libnl \
     libwfdaac_vendor
 
-include vendor/xiaomi/sdm845-common-extra/wfd.mk
+PRODUCT_BOOT_JARS += \
+    WfdCommon
