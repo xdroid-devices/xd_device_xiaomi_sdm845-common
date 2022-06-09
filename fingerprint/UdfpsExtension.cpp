LineageOS/android_device_xiaomi_sdm845-common

sdm845-common: fingerprint: Use dim layer kernel defined value
Change-Id: I5a005af972f6608ad5a69b10f3960bb3afcc6f92

fingerprint/UdfpsExtension.cpp
@@ -15,10 +15,11 @@
  */
 
 #include <compositionengine/UdfpsExtension.h>
+#include <drm/sde_drm.h>
 
 uint32_t getUdfpsZOrder(uint32_t z, bool touched) {
     if (touched) {
-        z |= 0x20000000u;
+        z |= UDFPS_PRESSED_LAYER_ZORDER;
     }
 
     return z;

xdroid-devices/xd_device_xiaomi_sdm845-common

fingerprint/UdfpsExtension.cpp
@@ -17,10 +17,11 @@
 #include <stdint.h>
 #include <drm/sde_drm.h>
 #include <compositionengine/UdfpsExtension.h>
+#include <drm/sde_drm.h>
 
 uint32_t getUdfpsZOrder(uint32_t z, bool touched) {
     if (touched) {
-        z |= 0x20000000u;
+        z |= FOD_PRESSED_LAYER_ZORDER;
     }
 
     return z;