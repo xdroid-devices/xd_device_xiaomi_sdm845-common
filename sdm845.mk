43bec879149aecf5fd41ae778d5b638f35f6a59f
Kyuofox <88403766@qq.com>
2022-06-11 21:51:14 +0800

    sdm845-common: Bring up xdroidOSS
    Signed-off-by: Kyuofox <88403766@qq.com>

--- sdm845.mk
+++ sdm845.mk
@@ -19,7 +19,7 @@ PRODUCT_COPY_FILES += \
 # Overlays
 DEVICE_PACKAGE_OVERLAYS += \
     $(LOCAL_PATH)/overlay \
-    $(LOCAL_PATH)/overlay-kscope
+    $(LOCAL_PATH)/overlay-xdroid
 
 PRODUCT_ENFORCE_RRO_TARGETS := *
 
